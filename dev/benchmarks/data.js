faa7061aeefce04ce1a0481121096d3b7eafa9ce
github-action-benchmark <github@users.noreply.github.com>
2026-01-06 12:29:36 +0000

    add BareJS Performance (Latency) (customSmallerIsBetter) benchmark result for 81087399dd160dc2c19a27f3216be1aa6080b64c

---
+++ dev/benchmarks/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1767702576362,
+  "repoUrl": "https://github.com/xarhang/bareJS",
+  "entries": {
+    "BareJS Performance (Latency)": [
+      {
+        "commit": {
+          "author": {
+            "email": "xarhang@hotmail.com",
+            "name": "xarhang",
+            "username": "xarhang"
+          },
+          "committer": {
+            "email": "xarhang@hotmail.com",
+            "name": "xarhang",
+            "username": "xarhang"
+          },
+          "distinct": true,
+          "id": "81087399dd160dc2c19a27f3216be1aa6080b64c",
+          "message": "docs: fix action github",
+          "timestamp": "2026-01-06T19:29:08+07:00",
+          "tree_id": "23db8815c0da5b319f0f116ca0a13159ad57ec44",
+          "url": "https://github.com/xarhang/bareJS/commit/81087399dd160dc2c19a27f3216be1aa6080b64c"
+        },
+        "date": 1767702576062,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "BareJS",
+            "value": 0,
+            "unit": "undefined"
+          },
+          {
+            "name": "Elysia",
+            "value": 0,
+            "unit": "undefined"
+          },
+          {
+            "name": "Hono",
+            "value": 0,
+            "unit": "undefined"
+          }
+        ]
+      }
+    ]
+  }
+}
